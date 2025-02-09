52b39204183190ed577498d20ab0deaaa6ea5378
Serdargunes <95936184+serdargunes@users.noreply.github.com>
2025-02-09 15:00:10 +0300

    Page Design

---
+++ src/components/Design/Design.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function Design() {
+  return (
+    <View>
+        <Text>Serdar</Text>
+    </View>
+  )
+}
+
+export default Design;
